4a7479900facd7de490d1fcfcfcf7b017c0f0bd2
nori-k <norikatsu.kamiya@gmail.com>
2019-09-25 16:08:03 +0900

    code fizzbuzz

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+// Fizz Buzz Code
+function fizzbuzz(params: void) {
+  for (let index = 0; index < 100; index++) {
+    let result: any = index;
+    if (index % 3 === 0 && index % 5 === 0) {
+      result = "FizzBuzz";
+    } else if (index % 3 === 0) {
+      result = "Fizz";
+    } else if (index % 5 === 0) {
+      result = "Buzz";
+    }
+    console.log(result);
+  }
 }
 
-console.log(hello("Type Script!!"));
+fizzbuzz();
